cd02dad0a6479964d8d15e80ce3bf1feab047087
Davi Rocha <102175365+Pongo013@users.noreply.github.com>
2025-03-28 21:09:39 +0000

    Finalização, todos os códigos estão como solicitado

--- xadrez.c
+++ xadrez.c
@@ -9,17 +9,22 @@ void moverTorre(int casas) {
 
 void moverBispo(int casas) {//precisa ser feito com loop aninhado
     if (casas > 0) {
-        for (int c = 0; c < casas; c++) {
-            for (int d = 0; d < casas; d++) {
+        int var = casas % 2;
+        for (int c = casas; c % 2 == var; c++) {
+            for (int d = casas; d % 2 == var; d++) {
                 printf("Bispo para Direita\n");
             }
             printf("Bispo para Cima\n");
         }
+        moverBispo(casas - 1);
     }
 }
 
 void moverRainha(int casas) {
-
+    if (casas > 0) {
+        printf("Rainha para Esquerda\n");
+        moverRainha(casas - 1);
+    }
 }
 
 int main() {
@@ -30,32 +35,13 @@ int main() {
     printf("=== Movimentação da Rainha:\n");
     moverRainha(8);
     printf("=== Movimentação do Cavalo:\n");
-    //movimento do cavalo é pra ser recursivo, mas usa loops aninhacos com multiplas variaveis/condicoes
-    return 0;
-
-    /*
-    //movimento da torre
-    printf("=== Movimento da torre:\n");
-    for (int t = 0; t < 5; t++) {
-        printf("Torre para Direita\n");
-    }
-    //movimento do bispo
-    printf("=== Movimento do bispo:\n");
-    for (int b = 0; b < 5; b++) {
-        printf("Bispo para Cima e Direita\n");
-    }
-    //movimento da rainha
-    printf("=== Movimento da Rainha:\n");
-    for (int r = 0; r < 8; r++) {
-        printf("Rainha para Esquerda\n");
-    }
-    //movimento do cavalo
-    printf("=== Movimento do Cavalo:\n");
-    for (int c = 0; c < 1; c++) {
-        for (c; c < 2; c++) {
-            printf("Cavalo para Baixo\n");
+    for (int c = 0, d = 0; c <= 1 && d < 1; c++) {
+        //a verificação do "c" é <= 1, pois ele é iniciado em 0. ou seja, o loop irá executar o comando quando "c" for 0 e 1.
+        printf("Cavalo para Cima\n");
+        for (d; c == 1 && d < 1; d++) {
+            //a verificação é se c == 1, pois quando "c" andar as duas casas, seu valor será 1, e "d" poderá mover para o lado
+            printf("Cavalo para Direita\n");
         }
-        printf("Cavalo para Esquerda\n");
     }
-    */
+    return 0;
 }
